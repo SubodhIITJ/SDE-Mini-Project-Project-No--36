b0fecb903cbfea386b9d5133cb42246428949f05
SubodhIITJ <m23csa531@iitj.ac.in>
2024-09-28 21:01:39 +0530

    Add files via upload

---
+++ about.php
@@ -0,0 +1,12 @@
+<a href="<?php echo web_root; ?>index.php" ><img src="images/pic1.png" style="width: 100%;object-fit: contain;height: 100%" alt="IMG"></a> 
+
+<h1><?php echo $title;?></h1>
+<p>Indian Institute of Technology Jodhpur is one of the top-ranked Engineering institutes in India. IIT Jodhpur rankings have shown an increasing trend since 2020. IITJ is ranked 30th among the top Engineering colleges in India by NIRF 2023. Additionally, it has been ranked 66th in the overall category by NIRF 2023.</p>
+
+<p>Indian Institute of Technology Jodhpur, through its 10 departments and 3 schools of study, offers UG, PG, PG diploma, and PhD courses. At the UG level, the IIT Jodhpur offers a four-year BTech in eight specialisations and a BSc in two specialisations. Indian Institute of Technology Jodhpur offers the students two-year MSc, MTech, and MBA courses at the PG level. It also offers integrated courses like MSc MTech and MTech PhD courses to the students. Further, at the doctoral level, the institute offers PhD across Core Sciences, Engineering Science, Humanities, and Social Sciences streams.</p>
+
+<p>BTech and MBA are the flagship programmes of IIT Jodhpur. Through its School of Management & Entrepreneurship (SME), the institute offers a full-time, two-year MBA programme. IIT Jodhpur MBA programme comprises various subjects such as Macro Economics, Global Economy, Human Capital, Marketing, and Finance, among others. In the second year, the institute offers two choices to students leading to either an MBA degree or the MBA Technology degree.</p>
+
+<p>Some of the popular BTech specialisations offered at IIT Jodhpur include Computer Science & Engineering, Electrical Engineering, Bioengineering, Civil & Infrastructure Engineering, and Artificial Intelligence & Data Science, among others. Tabulated below are the details about IIT Jodhpur popular programmes and their eligibility criteria and fees:</p>
+
+<p>IIT Jodhpur admissions are based on the entrance exam. The IIT Jodhpur admission process is conducted through online registrations. Candidates must fill out the IIT Jodhpur application form before the last date. IIT Jodhpur entrance exams are JEE Main, JEE Advanced, GATE, CAT, and IIT JAM.</p>
---
+++ home.php
@@ -0,0 +1,41 @@
+<!-- <h1><?php echo $title;?></h1> -->
+
+<div class="col-lg-12">
+<!-- <img src="images/pic1.png" style="width: 100%;object-fit: contain;height: 100%" alt="IMG">  -->
+<!-- <a class="navbar-brand"  href="<?php echo web_root; ?>index.php" ><img src="images/pic1.png" style="width: 100%;object-fit: contain;height: 100%" alt="IMG"></a>  -->
+<a href="<?php echo web_root; ?>index.php" ><img src="images/pic1.png" style="width: 100%;object-fit: contain;height: 100%" alt="IMG"></a> 
+<h4>IIT Jodhpur Highlights</h4>
+    <p> Indian Institute of Technology Jodhpur is one of the top-ranked Engineering institutes in India. IIT Jodhpur rankings have shown an increasing trend since 2020. IITJ is ranked 30th among the top Engineering colleges in India by NIRF 2023. Additionally, it has been ranked 66th in the overall category by NIRF 2023.</p> 
+</div>
+<div  class="col-lg-6">
+  <h4>Vision</h4> 
+  <p>Center of Academic Excellence Delivering Quality Education to everyone in the India.</p>
+  <h4>IIT Jodhpur Popular Programmes</h4>
+  <p> Indian Institute of Technology Jodhpur, through its 10 departments and 3 schools of study, offers UG, PG, PG diploma, and PhD courses. At the UG level, the IIT Jodhpur offers a four-year BTech in eight specialisations and a BSc in two specialisations. Indian Institute of Technology Jodhpur offers the students two-year MSc, MTech, and MBA courses at the PG level. It also offers integrated courses like MSc MTech and MTech PhD courses to the students. Further, at the doctoral level, the institute offers PhD across Core Sciences, Engineering Science, Humanities, and Social Sciences streams.</p>
+
+  <p> BTech and MBA are the flagship programmes of IIT Jodhpur. Through its School of Management & Entrepreneurship (SME), the institute offers a full-time, two-year MBA programme. IIT Jodhpur MBA programme comprises various subjects such as Macro Economics, Global Economy, Human Capital, Marketing, and Finance, among others. In the second year, the institute offers two choices to students leading to either an MBA degree or the MBA Technology degree.</p>
+
+  <!-- <p>Popular BTech specialisations offered at IIT Jodhpur include Computer Science & Engineering, Electrical Engineering, Bioengineering, Civil & Infrastructure Engineering, and Artificial Intelligence & Data Science, among others.</p> -->
+
+</div>
+<div  class="col-lg-6">
+ 
+<h4>IIT Jodhpur Admission Process</h4> 
+<ol>
+    <li>IIT Jodhpur admissions are based on the entrance exam.</li> 
+    <li>The IIT Jodhpur admission process is conducted through online registrations.</li> 
+    <li>Candidates must fill out the IIT Jodhpur application form before the last date.</li> 
+    <li>IIT Jodhpur entrance exams are JEE Main, JEE Advanced, GATE, CAT, and IIT JAM.</li> 
+      
+</ol>
+<h4>IIT Jodhpur Placements</h4> 
+<ol>
+    <li>More than 280 companies visited the campus and 380 offers were made during IIT Jodhpur placements 2024. </li> 
+    <li>Further, the placement rate recorded during BTech, MTech and MBA placements 2024 stood at 86%, 65.77% and 85.29%, respectively.</li> 
+    <li>Earlier, the highest and average package offered during IIT Jodhpur MBA placements 2023 stood at INR 23.8 LPA and INR 13.03 LPA, respectively. </li> 
+    <li>Further, the median package offered during IIT Jodhpur MBA placements 2023 stood at INR 12 LPA. </li> 
+      
+</ol>
+</div>
+ 
+
